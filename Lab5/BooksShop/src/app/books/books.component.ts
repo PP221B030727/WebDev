1d0801054ed53de1f398b3005bf9ff6e6c872eb1
Нурсултан Туругельдиев <nursultanturugeldiev@MacBook-Air-Nursultan.local>
2023-02-26 00:16:13 +0600

    Interpolation

--- Lab5/BooksShop/src/app/books/books.component.ts
+++ Lab5/BooksShop/src/app/books/books.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
-export class BooksComponent {
+export class BooksComponent{
+  src : string = 'https://m.media-amazon.com/images/I/41xShlnTZTL.jpg'; // ссылка на фото
+  name : string = 'Clean Code';//Добавляем елемент имя
+  author : string = 'Robert C Martin'; // Доьавляем елемент автор
+
+
+  src_2 : string = 'https://miro.medium.com/max/362/0*C0krYKx7Ws9dSsPI.jpg';
+  name_2 : string = 'Pragmatic Programmer';
+  author_2 : string = 'David Thomas';
+
+
+
 
 }

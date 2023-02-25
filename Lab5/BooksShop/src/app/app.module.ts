47b3d74cab82b7ed1dd9717643bc6508703d1600
Нурсултан Туругельдиев <nursultanturugeldiev@MacBook-Air-Nursultan.local>
2023-02-25 23:57:26 +0600

    ng generate Books

--- Lab5/BooksShop/src/app/app.module.ts
+++ Lab5/BooksShop/src/app/app.module.ts
@@ -2,9 +2,10 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
+import { BooksComponent } from './books/books.component';
 @NgModule(
     {
-        declarations:[AppComponent],//определяю компоненты 1 шаг действий 
+        declarations:[AppComponent, BooksComponent],//определяю компоненты 1 шаг действий 
         imports:[BrowserModule], // Для того что бы иметь в себе все браузеры 
         bootstrap:[AppComponent]  //Компонент начальной загрузкий  
 

2d0fda41dafd93b5962c163d91c48d23fcb1d35b
Нурсултан Туругельдиев <nursultanturugeldiev@MacBook-Air-Nursultan.local>
2023-03-01 00:59:52 +0600

    *ngFor - example

--- Lab5/BooksShop/src/app/books/books.component.ts
+++ Lab5/BooksShop/src/app/books/books.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Book {
+  src : string,
+  name : string,
+  author : string
+}
+
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent{
-  src : string = 'https://m.media-amazon.com/images/I/41xShlnTZTL.jpg'; // ссылка на фото
-  name : string = 'Clean Code';//Добавляем елемент имя
-  author : string = 'Robert C Martin'; // Доьавляем елемент автор
+  books : Book[] = [
+    {
+      src : 'https://m.media-amazon.com/images/I/41xShlnTZTL.jpg', // ссылка на фото
+      name : 'Clean Code',//Добавляем елемент имя
+      author : 'Robert C Martin', // Доьавляем елемент автор
+    },
+    {
+      src : 'https://miro.medium.com/max/362/0*C0krYKx7Ws9dSsPI.jpg',
+      name : 'Pragmatic Programmer',
+      author : 'David Thomas',
+    }  
+  ]
+  
 
 
-  src_2 : string = 'https://miro.medium.com/max/362/0*C0krYKx7Ws9dSsPI.jpg';
-  name_2 : string = 'Pragmatic Programmer';
-  author_2 : string = 'David Thomas';
+  
 
 
   isShowing : boolean = false;

230c3006dc4c99344fa4798a144329d589ce5bb9
Нурсултан Туругельдиев <nursultanturugeldiev@MacBook-Air-Nursultan.local>
2023-03-05 16:45:11 +0600

    angulart routing with ng generate module app-routing --flat --module=app

---
+++ Lab5/BooksShop/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { CartComponent } from './cart/cart.component';
+
+
+const routes : Routes = [
+  {path:'',component:BooksComponent},
+  {path:'cart',component:CartComponent}
+
+];
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports : [RouterModule],
+})
+export class AppRoutingModule { }
--- Lab5/BooksShop/src/app/app.module.ts
+++ Lab5/BooksShop/src/app/app.module.ts
@@ -8,10 +8,12 @@ import { BookComponent } from './books/book/book.component';
 import { BooksService } from "./books/books.service";
 import { BooksModule } from "./books/books.module";
 import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule(
     {
         declarations:[AppComponent, CartComponent],//определяю компоненты 1 шаг действий 
-        imports:[BrowserModule , FormsModule , BooksModule], // Для того что бы иметь в себе все браузеры 
+        imports:[BrowserModule , FormsModule , BooksModule, AppRoutingModule], // Для того что бы иметь в себе все браузеры 
         bootstrap:[AppComponent] , //Компонент начальной загрузкий  
         // providers:[BooksService]
     }

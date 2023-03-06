c18367ff56d892db5de6d43c55c13452ee9dc72e
Нурсултан Туругельдиев <nursultanturugeldiev@MacBook-Air-Nursultan.local>
2023-03-06 17:45:07 +0600

    adding photos

--- Lab6/Album/src/app/app-routing.module.ts
+++ Lab6/Album/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AlbumPhotosComponent } from './albums/album-photos/album-photos.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'about',component:AboutComponent},
-  {path:'albums',component:AlbumsComponent}
+  {path:'albums',component:AlbumsComponent},
+  {path: 'albums/photos', component:AlbumPhotosComponent}
+  // {path: 'albums/:albumId', component: AlbumPhotosComponent },
+  // {path: 'albums/album/:albumId', component: AlbumPhotosComponent }
 ];
 
 @NgModule({

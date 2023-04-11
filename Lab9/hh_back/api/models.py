ec0128bf248af57e2da6ab0c09483b0998fb8d40
Нурсултан Туругельдиев <nursultanturugeldiev@MacBook-Air-Nursultan.local>
2023-04-11 20:19:10 +0600

    adding models company vacancy

--- Lab9/hh_back/api/models.py
+++ Lab9/hh_back/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)
+    city = models.CharField(max_length=128, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.name
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(null=True, blank=True)
+    salary = models.FloatField(null=True, blank=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+
+

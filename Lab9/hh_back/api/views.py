20dc8183cb3d08e37d06575f90722d3002b84954
Нурсултан Туругельдиев <nursultanturugeldiev@MacBook-Air-Nursultan.local>
2023-04-12 13:26:29 +0600

    ADDING CRUD

--- Lab9/hh_back/api/views.py
+++ Lab9/hh_back/api/views.py
@@ -4,19 +4,46 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from api.models import *
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+
+
+
 def index(request):
     return HttpResponse("<h1>Api</h1>")
 
 
-
+@csrf_exempt
 def companies(request):
     companies = Company.objects.all()
     companies_json = [i.to_json() for i in companies]
-    return JsonResponse(companies_json, safe=False)
+
+    if(request.method == 'GET'):
+        return JsonResponse(companies_json, safe=False)
+    if(request.method == 'POST'):
+        # data = json.loads(request.body)
+        # company_name = data.get('name','New Name')
+        # company_description = data.get('description','new description')
+        # company_city = data.get('city','new city')
+        # company_address = data.get('address','new address')
+        # new_company = Company.objects.create(
+        #     name=company_name,
+        #     description=company_description,
+        #     city=company_city,
+        #     company_address=company_address
+        # )
+        # return JsonResponse(new_company.to_json(), safe=False)
+        return JsonResponse({
+            'name' : "Hello World"
+        })
+
+
 
 def vacancies(request):
     vacancies = Vacancy.objects.all()
     vacancies_json = [i.to_json() for i in vacancies]
+
     return JsonResponse(vacancies_json, safe=False)
 
 def get_company(request , id):
